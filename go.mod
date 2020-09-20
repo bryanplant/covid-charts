6c35ad3d0afaf0e6f69aece77fde0b4534aa580e
Bryan Plant <bryanplant@gmail.com>
2020-09-19 20:00:30 -0700

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bryanplant/covid-charts
+
+go 1.13
+
+require github.com/GoogleCloudPlatform/functions-framework-go v1.1.0 // indirect
---
+++ src/constants.go
@@ -0,0 +1,41 @@
+package src
+
+const(
+	Countries = "countries"
+	XStat     = "x_stat"
+	YStat    = "y_stat"
+	DataFile = "resources/owid-covid-data.csv"
+
+	IsoCode = "iso_code"
+	Continent = "continent"
+	Location = "location"
+	Date = "date"
+
+	TotalCases = "total_cases"
+	NewCases = "new_cases"
+	NewCasesSoothed = "new_cases_smoothed"
+	TotalDeaths = "total_deaths"
+	NewDeaths = "new_deaths"
+	NewDeathsSmoothed = "new_deaths_smoothed"
+	TotalCasesPerMillion = "total_cases_per_million"
+	NewCasesPerMillion = "new_cases_per_million"
+	NewCasesSmoothedPerMillion = "new_cases_smoothed_per_million"
+	TotalDeathsPerMillion = "total_deaths_per_million"
+	NewDeathsPerMillion = "new_deaths_per_million"
+	NewDeathsSmoothedPerMillion = "new_deaths_smoothed_per_million"
+	NewTests = "new_tests"
+	TotalTests = "total_tests"
+	TotalTestsPerThousand = "total_tests_per_thousand"
+	NewTestsPerThousand = "new_tests_per_thousand"
+	NewTestsSmoothed = "new_tests_smoothed"
+	NewTestsSmoothedPerThousand = "new_tests_smoothed_per_thousand"
+	TestsPerCase = "tests_per_case"
+	PositiveRate = "positive_rate"
+
+	TestUnits = "tests_units"
+	StringencyIndex = "stringency_index"
+	Population = "population"
+	PopulationDensity = "population_density"
+	MedianAge = "median_age"
+)
+
